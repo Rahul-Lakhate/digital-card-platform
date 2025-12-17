e7dea41be701b01a05e7241f695ccb1e4f3b257c
Rahul <rahul.lakhate@gmail.com>
2025-12-17 17:55:23 +0530

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,6 @@
-config.py
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+JWT_SECRET = os.getenv("JWT_SECRET", "secret")
+RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
+RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
